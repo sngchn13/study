68d397cb2fc29220118aadf22873225cd48a488f
seungchan <tmdcks3489@gmail.com>
2026-09-23 03:01:01 +0900

    tile

--- unit_path.gd
+++ unit_path.gd
@@ -2,8 +2,6 @@ class_name UnitPath
 extends TileMapLayer
 
 @export var grid: Grid
-@export var tile_source_id := 0
-@export var tile_atlas_coords := Vector2i.ZERO
 
 var _pathfinder: PathFinder
 var current_path: Array[Vector2i] = []
@@ -14,8 +12,7 @@ func initialize(walkable_cells: Array[Vector2i]) -> void:
 func draw(cell_start: Vector2i, cell_end: Vector2i) -> void:
 	clear()
 	current_path = _pathfinder.calculate_point_path(cell_start, cell_end)
-	for cell in current_path:
-		set_cell(cell, tile_source_id, tile_atlas_coords)
+	set_cells_terrain_connect(current_path, 0, 0)
 
 func stop() -> void:
 	_pathfinder = null
